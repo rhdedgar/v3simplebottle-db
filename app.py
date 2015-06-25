5978ebd793c9d832e34f855fc03f53c63e36a27e
Steven Pousty <spousty@redhat.com>
2015-06-24 19:23:44 -0700

    debug connection

--- app.py
+++ app.py
@@ -2,18 +2,19 @@ __author__ = 'spousty'
 
 import psycopg2
 from bottle import route, run
+import os
 
 @route('/')
 def index():
-    return "<h1> hello OpenShift Ninja with DB</h1>"
-    
+	return "<h1> hello OpenShift Ninja with DB</h1>"
+
 @route('/db')
 def dbexample():
 	try:
-	#TODO change the connection info to env variables
-		conn = psycopg2.connect("dbname='db' user=os.environ.get('POSTGRESQL_USER') host=os.environ.get('POSTGRESQL_92_CENTOS7_SERVICE_HOST') password=os.environ.get('POSTGRESQL_PASSWORD')")
+		#TODO change the connection info to env variables
+		conn = psycopg2.connect(dbname='db', user=os.environ.get('POSTGRESQL_USER'), host=os.environ.get('POSTGRESQL_92_CENTOS7_SERVICE_HOST'), password=os.environ.get('POSTGRESQL_PASSWORD'))
 	except:
-		return "<h1>DB ERROR - DANGER DANGER"
+		return os.environ.get('POSTGRESQL_USER') + "  " + os.environ.get('POSTGRESQL_92_CENTOS7_SERVICE_HOST')
 	
 	cur = conn.cursor()
 	cur.execute("""SELECT * from users""")
@@ -22,8 +23,8 @@ def dbexample():
 	result_string = "Here are your results: \n"
 	for row in rows:
 		result_string += row[0] + "\n"
-    	
+
 	return "<h2> " + result_string + "</h2>"
 
 if __name__ == '__main__':
-    run(host='0.0.0.0', port=8080)
+	run(host='0.0.0.0', port=8080)
