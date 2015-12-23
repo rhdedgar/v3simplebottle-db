309d0c9a6465cc5b0e6dd7b0073340f1f4f72e82
Doug Edgar <dedgar@redhat.com>
2015-12-23 13:12:54 -0800

    updated

--- app.py
+++ app.py
@@ -6,14 +6,13 @@ import os
 
 @route('/')
 def index():
-  test_string = '''
-  <button type=\"button\" onclick=\"alert(\'Hello world!\')\">Grade 1</button>
-  <button type=\"button\" onclick=\"alert(\'Hello world!\')\">Grade 2</button>
-  <button type=\"button\" onclick=\"alert(\'Hello world!\')\">Grade 3</button>
-  <button type=\"button\" onclick=\"alert(\'Hello world!\')\">Grade 4</button>
-  <button type=\"button\" onclick=\"alert(\'Hello world!\')\">Grade 5</button>
-  <button type=\"button\" onclick=\"alert(\'Hello world!\')\">Grade 6</button>'''
-  
+  grade1 = "<button type=\"button\" onclick=\"alert(\'Hello world!\')\">Grade 1</button>"
+  grade2 = "<button type=\"button\" onclick=\"alert(\'Hello world!\')\">Grade 2</button>"
+  grade3 = "<button type=\"button\" onclick=\"alert(\'Hello world!\')\">Grade 3</button>"
+  grade4 = "<button type=\"button\" onclick=\"alert(\'Hello world!\')\">Grade 4</button>"
+  grade5 = "<button type=\"button\" onclick=\"alert(\'Hello world!\')\">Grade 5</button>"
+  grade6 = "<button type=\"button\" onclick=\"alert(\'Hello world!\')\">Grade 6</button>"
+  test_string = "<h3>" + grade1 + grade2 + grade3 + grade4 + grade5 + grade6 + "</h3>"
   return test_string 
 
 @route('/db')
