fa39d9750d8e8fdbbd524f1084edf715c84560fa
Doug Edgar <dedgar@redhat.com>
2017-02-19 14:57:33 -0800

    updating

--- app.py
+++ app.py
@@ -31,10 +31,11 @@ def db_level(selection=None, level=None):
     cur.execute("""SELECT kanj, von, vkun, transl FROM info WHERE %s = %s""" % (selection, level))
 
     rows = cur.fetchall()
-    result_string = "<h2>Here are your results: </h2>"
-
+    result_string = '' #"<h2>Here are your results: </h2>"
+    kanji_list = []
     for row in rows:
-        result_string += "<h3>" + row[0] + ", " + row[1] + ", " + row[2] + ", "  + row[3] + "</h3>"
+        result_string += row[0] + ", " + row[1] + ", " + row[2] + ", "  + row[3]
+        kanji_list.append(row[0])
 
     return render_template('flashcard.html', kanji=result_string)
 
@@ -45,5 +46,11 @@ def db_kanji(selection=None, level=None, kanji=None):
     print(selection, level, kanji)
 
 
+def split_space(string):
+    """ For use with jinja2 filters. """
+    return string.strip().split()
+
+
 if __name__ == '__main__':
+    APP.jinja_env.filters['split_space'] = split_space
     APP.run(host='0.0.0.0', port=8080, debug=True)
