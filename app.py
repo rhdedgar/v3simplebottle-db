2d77aafde83a0c9ff4f9d6bce4bd426f8665bbbe
Doug Edgar <dedgar@redhat.com>
2017-03-04 12:44:59 -0800

    updating

--- app.py
+++ app.py
@@ -32,13 +32,13 @@ def db_level(selection=None, level=None):
 
     rows = cur.fetchall()
     result_string = ''
-    k_list = []
+    kanji_list = []
     for row in rows:
         result_string += row[0] + ", " + row[1] + ", " + row[2] + ", "  + row[3]
-        k_list.append(row[0])
+        kanji_list.append(row[0])
 
     return render_template('kanji_list.html',
-                           kanji_list=k_list,
+                           k_list=kanji_list,
                            u_level=level,
                            u_selection=selection
                           )
@@ -54,11 +54,43 @@ def db_kanji(selection=None, level=None, kanji=None):
         level = request.form.split(' ')[1]
         kanji = request.form.split(' ')[2]
 
+#    conn = psycopg2.connect(database='kanji', user=os.environ.get('POSTGRESQL_USER'),\
+#                            host=os.environ.get('POSTGRESQL_SERVICE_HOST'),\
+#                            password=os.environ.get('POSTGRESQL_PASSWORD'))
+#    cur = conn.cursor()
+#    cur.execute("""SELECT kanj, von, vkun, transl FROM info WHERE kanj = '%s'""" % kanji)
+#
+#    rows = cur.fetchall()
+#
+#    result_string = ""
+#    for row in rows:
+#        result_string += row[0] + ", " + row[1] + ", " + row[2] + ", "  + row[3]
+
+    kanji_query = 'SELECT kanj, von, vkun, transl FROM info WHERE kanj = %s' % kanji
+    res_string = get_results(kanji_query)
+
+    list_query = 'SELECT kanj, von, vkun, transl FROM info WHERE %s = %s' % (selection, level)
+    kanji_list = get_results(list_query)
+
+    return render_template('flashcard.html',
+                           res_string=res_string,
+                           k_list=kanji_list
+                          )
+
+
+def split_space(string):
+    """ For use with jinja2 filters. """
+    return string.strip().split()
+
+
+def get_results(query):
+    """ Get database results for supplied query. """
+
     conn = psycopg2.connect(database='kanji', user=os.environ.get('POSTGRESQL_USER'),\
                             host=os.environ.get('POSTGRESQL_SERVICE_HOST'),\
                             password=os.environ.get('POSTGRESQL_PASSWORD'))
     cur = conn.cursor()
-    cur.execute("""SELECT kanj, von, vkun, transl FROM info WHERE kanj = '%s'""" % kanji)
+    cur.execute(query)
 
     rows = cur.fetchall()
 
@@ -66,13 +98,7 @@ def db_kanji(selection=None, level=None, kanji=None):
     for row in rows:
         result_string += row[0] + ", " + row[1] + ", " + row[2] + ", "  + row[3]
 
-    return render_template('flashcard.html', res_string=result_string)
-
-
-def split_space(string):
-    """ For use with jinja2 filters. """
-    return string.strip().split()
-
+    return result_string
 
 if __name__ == '__main__':
     APP.jinja_env.filters['split_space'] = split_space
