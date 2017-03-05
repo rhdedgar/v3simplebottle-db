b005358f3faf1b019bc9b37f234ad34d75d7a44a
Doug Edgar <dedgar@redhat.com>
2017-03-05 12:31:34 -0800

    updating

--- app.py
+++ app.py
@@ -54,27 +54,33 @@ def db_kanji(selection=None, level=None, kanji=None):
         level = request.form.split(' ')[1]
         kanji = request.form.split(' ')[2]
 
-#    conn = psycopg2.connect(database='kanji', user=os.environ.get('POSTGRESQL_USER'),\
-#                            host=os.environ.get('POSTGRESQL_SERVICE_HOST'),\
-#                            password=os.environ.get('POSTGRESQL_PASSWORD'))
-#    cur = conn.cursor()
-#    cur.execute("""SELECT kanj, von, vkun, transl FROM info WHERE kanj = '%s'""" % kanji)
-#
-#    rows = cur.fetchall()
-#
-#    result_string = ""
-#    for row in rows:
-#        result_string += row[0] + ", " + row[1] + ", " + row[2] + ", "  + row[3]
-
     kanji_query = """SELECT kanj, von, vkun, transl FROM info WHERE kanj = '%s'""" % kanji
     res_string = get_results(kanji_query)
 
     list_query = 'SELECT kanj FROM info WHERE %s = %s' % (selection, level)
     kanji_list = get_results(list_query)
 
+    current_pos = kanji_list.index(res_string)
+
+    if current_pos == kanji_list[-1]:
+        next_kanji = kanji_list[0]
+        prev_kanji = kanji_list[current_pos - 1]
+
+    elif current_pos == kanji_list[0]:
+        next_kanji = kanji_list[current_pos + 1]
+        prev_kanji = kanji_list[-1]
+
+    else:
+        next_kanji = kanji_list[current_pos + 1]
+        prev_kanji = kanji_list[current_pos - 1]
+
     return render_template('flashcard.html',
                            res_string=res_string,
-                           k_list=kanji_list
+                           k_list=kanji_list,
+                           u_level=level,
+                           u_selection=selection,
+                           p_kanji=prev_kanji,
+                           n_kanji=next_kanji
                           )
 
 
@@ -88,16 +94,14 @@ def get_results(query):
     cur.execute(query)
 
     rows = cur.fetchall()
-
     result_string = ""
+    result_list = []
 
     for row in rows:
-
         if len(row) == 4:
             result_string += row[0] + ", " + row[1] + ", " + row[2] + ", "  + row[3]
-
         else:
-            result_string += row[0]
+            result_list.append(row[0])
 
     return result_string
 
